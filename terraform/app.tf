6acbef4f049a62243c3e4cd7cca0f4123ccb9054
saurabh <singla86@gmail.com>
2020-10-12 22:15:36 +0800

    Defined parameterized terraform config files to bring web application up on Load balancer on AWS Note: It is assumed that docker image for web is already uplaoded to the ECR repo and same has been referenced in app.tf 3 containers are spinned up to achieve app resiliency

---
+++ terraform/app.tf
@@ -0,0 +1,72 @@
+resource "aws_ecr_repository" "techapp_ecr_repo" {
+  name = "techapp-ecr-repo"
+}
+resource "aws_ecs_cluster" "techapp_cluster" {
+  name = "techapp-cluster" 
+}
+resource "aws_ecs_task_definition" "techapp" {
+  family                   = "techapp-task-family"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "techapp-task-family",
+      "image": "612563676328.dkr.ecr.ap-southeast-1.amazonaws.com/techapp-ecr-repo:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "appTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "techappservice" {
+  name            = "techappservice"
+  cluster         = aws_ecs_cluster.techapp_cluster.id
+  task_definition = aws_ecs_task_definition.techapp.arn
+  launch_type     = "FARGATE"
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.techapp.family
+    container_port   = 3000
+  }
+
+  network_configuration {
+    subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id,aws_default_subnet.default_subnet_c.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.service_security_group.id]
+  }
+}
---
+++ terraform/lb.tf
@@ -0,0 +1,65 @@
+resource "aws_alb" "application_load_balancer" {
+  name               = "techapp-lb-tf"
+  load_balancer_type = "application"
+  subnets = [
+    aws_default_subnet.default_subnet_a.id,
+    aws_default_subnet.default_subnet_b.id,
+    aws_default_subnet.default_subnet_c.id
+  ]
+  security_groups = [aws_security_group.load_balancer_security_group.id]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id
+  health_check {
+    matcher = "200,301,302"
+    path = "/healthcheck/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
